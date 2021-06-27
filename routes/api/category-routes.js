b210f13643a910e747a7d2e8fdf2c2a047c15deb
Iman Bains <imanjitbains@yahoo.com>
2021-06-26 17:40:23 -0700

    Added product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,44 +1,44 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Category.findAll({
-      attributes: ['id', 'category_name'],
+      attributes: ["id", "category_name"],
       include: [{
               model: Product,
-              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+              attributes: ["id", "product_name", "price", "stock", "category_id"]
           }]
   }).then((category) => res.json(category))
     .catch((err) => res.status(500).json(err));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Category.findOne({
       where: {id: req.params.id},
-      attributes: ['id', 'category_name'],
+      attributes: ["id", "category_name"],
       include: [{
               model: Product,
-              attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+              attributes: ["id", "product_name", "price", "stock", "category_id"]
           }]
   }).then((category) => res.json(category))
     .catch((err) => res.status(400).json(err));
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Category.create({
       category_name: req.body.category_name
   }).then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   Category.update(req.body, {
       where: {id: req.params.id}
   }).then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Category.destroy({
       where: {id: req.params.id}
   }).then((category) => res.status(200).json(category))
